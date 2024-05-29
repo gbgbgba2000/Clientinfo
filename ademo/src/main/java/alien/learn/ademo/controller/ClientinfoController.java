0cd21f31fdd79190877d37a03d35e14eab6bed16
gbgbgba2000 <gbgbgba2000@gmail.com>
2024-05-29 09:58:08 +0800

    第二版
    修正service 使用interface

--- ademo/src/main/java/alien/learn/ademo/controller/ClientinfoController.java
+++ ademo/src/main/java/alien/learn/ademo/controller/ClientinfoController.java
@@ -1,4 +1,4 @@
-package alien.learn.ademo.controller;
+/*package alien.learn.ademo.controller;
 
 import java.io.IOException;
 import java.util.List;
@@ -81,4 +81,83 @@ public class ClientinfoController {
         fileName, bucketName, blobInfo.getName());
   }
 
+}*/
+
+package alien.learn.ademo.controller;
+
+import java.io.IOException;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import alien.learn.ademo.GcpStorageConfig;
+import alien.learn.ademo.entity.Clientinfo;
+import alien.learn.ademo.service.ClientinfoService;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.storage.BlobInfo;
+import com.google.cloud.storage.StorageOptions;
+import com.google.cloud.storage.Storage;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ClassPathResource;
+
+@RestController
+@RequestMapping("/clientInfos")
+public class ClientinfoController {
+
+  @Autowired
+  private ClientinfoService service;
+
+  @Autowired
+  private GcpStorageConfig gcpStorageConfig;
+
+  private Storage storage;
+
+  @Autowired
+  public void init() throws IOException {
+    Resource resource = new ClassPathResource("credentials.json");
+    GoogleCredentials credentials = GoogleCredentials.fromStream(resource.getInputStream());
+    storage = StorageOptions.newBuilder().setCredentials(credentials).build().getService();
+  }
+
+  @PostMapping
+  public Clientinfo addClientInfo(@RequestBody Clientinfo clientInfo) {
+    return service.saveClientInfo(clientInfo);
+  }
+
+  @GetMapping
+  public List<Clientinfo> getAllClientInfos() {
+    return service.getAllClientInfos();
+  }
+
+  @GetMapping("/{id}")
+  public Clientinfo getClientInfoById(@PathVariable Integer id) {
+    return service.getClientInfoById(id);
+  }
+
+  @GetMapping("/byName/{name}")
+  public Clientinfo getClientInfoByName(@PathVariable String name) {
+    return service.getClientInfoByName(name);
+  }
+
+  @PutMapping
+  public Clientinfo updateClientInfo(@RequestBody Clientinfo clientInfo) {
+    return service.updateClientInfo(clientInfo);
+  }
+
+  @DeleteMapping("/{id}")
+  public void deleteClientInfo(@PathVariable Integer id) {
+    service.deleteClientInfo(id);
+  }
+
+  @PostMapping("/upload")
+  public String uploadFile(@RequestParam("file") MultipartFile file) throws IOException {
+    String bucketName = gcpStorageConfig.getBucket();
+    String fileName = file.getOriginalFilename();
+    BlobInfo blobInfo = storage.create(
+        BlobInfo.newBuilder(bucketName, fileName).build(),
+        file.getBytes());
+
+    return String.format("File %s uploaded to bucket %s as %s",
+        fileName, bucketName, blobInfo.getName());
+  }
 }
--- ademo/src/main/java/alien/learn/ademo/service/ClientinfoServiceImpl.java
+++ ademo/src/main/java/alien/learn/ademo/service/ClientinfoServiceImpl.java
@@ -1,58 +1,47 @@
-/*package alien.learn.ademo.service;
 
-//import alien.learn.ademo.dto.ClientinfoDto;
+package alien.learn.ademo.service;
+
 import alien.learn.ademo.entity.Clientinfo;
 import alien.learn.ademo.repository.ClientinfoRepository;
+//import alien.learn.ademo.service.ClientinfoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import java.util.List;
 
 @Service
-public class ClientinfoService {
+public class ClientinfoServiceImpl implements ClientinfoService {
 
     @Autowired
     private ClientinfoRepository repository;
 
+    @Override
     public Clientinfo saveClientInfo(Clientinfo clientInfo) {
         return repository.save(clientInfo);
     }
 
+    @Override
     public List<Clientinfo> getAllClientInfos() {
         return repository.findAll();
     }
 
+    @Override
     public Clientinfo getClientInfoById(Integer id) {
         return repository.findById(id).orElse(null);
     }
 
+    @Override
     public Clientinfo getClientInfoByName(String name) {
         return repository.findByName(name);
     }
 
+    @Override
     public Clientinfo updateClientInfo(Clientinfo clientInfo) {
         return repository.save(clientInfo);
     }
 
+    @Override
     public void deleteClientInfo(Integer id) {
         repository.deleteById(id);
     }
-
-}*/
-package alien.learn.ademo.service;
-
-import alien.learn.ademo.entity.Clientinfo;
-import java.util.List;
-
-public interface ClientinfoService {
-    Clientinfo saveClientInfo(Clientinfo clientInfo);
-
-    List<Clientinfo> getAllClientInfos();
-
-    Clientinfo getClientInfoById(Integer id);
-
-    Clientinfo getClientInfoByName(String name);
-
-    Clientinfo updateClientInfo(Clientinfo clientInfo);
-
-    void deleteClientInfo(Integer id);
 }
